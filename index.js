88fdd7873c6d8d89f28d4c00bdf10610779129d4
AeroCotton <ashwin10.krish@gmail.comm>
2025-04-04 22:42:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Hamburger menu
+
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar() {
+  sidenav.style.left = "0"
+}
+
+function closeNavbar() {
+    sidenav.style.left = "-65%"
+}
