8a85974b3ea37e799a0e900e9bfafccde1e33232
AeroCotton <ashwin10.krish@gmail.comm>
2025-04-05 16:19:53 +0530

    Just improved little bit features

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var sidenav = document.querySelector(".side-navbar")
 
 function showNavbar() {
   sidenav.style.left = "0"
+  document.getElementById("overlay").style.display = "block";
+  document.body.classList.add("no-scroll"); // Disable scrolling
 }
 
 function closeNavbar() {
     sidenav.style.left = "-65%"
+    document.getElementById("overlay").style.display = "none";
+    document.body.classList.remove("no-scroll"); // Enable scrolling again
 }
